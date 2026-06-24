401df7fea917115275221d48b9eb819fe81fdc28
wumiapis <wumiapis@gmail.com>
2026-06-24 15:07:56 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+This is a template repository used in the [Get Started with HCP Terraform tutorial collection](https://developer.hashicorp.com/terraform/tutorials/cloud-get-started)
+
+This repository contains the following files:
+
+- `terraform.tf`: Configures Terraform and the AWS provider.
+- `main.tf`: Defines one AWS EC2 instance.
+- `variables.tf`: Defines two variables named `instance_name` and `instance_type`, both with default values.
+- `outputs.tf`: Defines one output named `instance_hostname`.
---
+++ main.tf
@@ -0,0 +1,45 @@
+provider "aws" {
+  region = "us-west-2"
+}
+
+data "aws_availability_zones" "available" {
+  state = "available"
+}
+
+data "aws_ami" "ubuntu" {
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd-gp3/ubuntu-noble-24.04-amd64-server-*"]
+  }
+
+  owners = ["099720109477"] # Canonical
+}
+
+module "vpc" {
+  source  = "terraform-aws-modules/vpc/aws"
+  version = "5.19.0"
+
+  name = "learn-hcp-terraform"
+  cidr = "10.0.0.0/16"
+
+  azs             = data.aws_availability_zones.available.names
+  private_subnets = ["10.0.1.0/24", "10.0.2.0/24"]
+  public_subnets  = ["10.0.101.0/24"]
+
+  enable_dns_hostnames = true
+}
+
+
+resource "aws_instance" "app_server" {
+  ami           = data.aws_ami.ubuntu.id
+  instance_type = var.instance_type
+
+  vpc_security_group_ids = [module.vpc.default_security_group_id]
+  subnet_id              = module.vpc.private_subnets[0]
+
+  tags = {
+    Name = var.instance_name
+  }
+}
